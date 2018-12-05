4cee4cbcccde561f651e85380b2fff6b21c6c5d6
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-12-05 19:06:15 +0200

    Day2 pt2 solved.

---
+++ 2018/day2/pt2.py
@@ -0,0 +1,29 @@
+"""
+https://adventofcode.com/2018/day/1
+"""
+from typing import Tuple
+
+
+def get_diff_count(box_id1: str, box_id2: str) -> Tuple[int, str]:
+    """
+    Calculates amount of different symbols for two strings.
+    """
+    count = 0
+    common = ""
+    for i in range(len(box_id1)):
+        if box_id1[i] != box_id2[i]:
+            count += 1
+        else:
+            common += box_id1[i]
+
+    return count, common
+
+
+with open("./input.txt") as f:
+    box_ids = f.read().splitlines()
+
+    for box_id1 in box_ids:
+        for box_id2 in box_ids:
+            count, common = get_diff_count(box_id1, box_id2)
+            if count == 1:
+                print(f'common: {common}')
