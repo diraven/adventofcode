052bcc098c12fe72e046badd432fadda56c8d80e
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-12-06 09:29:32 +0200

    Day 3 pt2.

---
+++ 2018/day3/pt2.py
@@ -0,0 +1,60 @@
+"""
+https://adventofcode.com/2018/day/3
+"""
+import timeit
+from typing import Tuple, List
+
+
+def check_claim(
+        heat_map: List[List[int]],
+        claim: Tuple[int, int, int, int, int]
+) -> bool:
+    """
+    Checks if given claim is clean.
+    """
+    for x in range(int(claim[1]), int(claim[1]) + int(claim[3])):
+        for y in range(int(claim[2]), int(claim[2]) + int(claim[4])):
+            if heat_map[x][y] > 1:
+                return False
+    return True
+
+
+def run() -> None:
+    """
+    Main function.
+    """
+    heat_map = [[0 for x in range(1000)] for x in range(1000)]
+
+    with open("./input.txt") as f:
+        claims = []  # type: List[Tuple[int, int, int, int, int]]
+
+        # Original format: 1 @ 335,861: 14x10
+        # Parsed format: (
+        # 0: id,
+        # 1: origin_x,
+        # 2: origin_y,
+        # 3: size_x,
+        # 4: size_y
+        # )
+        for line in f.read().splitlines():
+            id_origin_size = line.split('@')
+            ident = id_origin_size[0].lstrip('#')
+            origin_size = id_origin_size[1].strip().split(': ')
+            origin = origin_size[0].split(',')
+            size = origin_size[1].split('x')
+            claims.append((int(ident), int(origin[0]), int(origin[1]),
+                           int(size[0]), int(size[1])))
+
+        # Fill out the heatmap.
+        for claim in claims:
+            for x in range(int(claim[1]), int(claim[1]) + int(claim[3])):
+                for y in range(int(claim[2]), int(claim[2]) + int(claim[4])):
+                    heat_map[x][y] += 1
+
+        # Now check which claim is not heated.
+        for claim in claims:
+            if check_claim(heat_map, claim):
+                print(claim)
+
+
+print(timeit.timeit(run, number=1))
