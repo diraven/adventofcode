abff7c45dc476ad5e54c99036076b70e7b01dbfd
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-12-14 12:04:19 +0200

    Day14 pt2 resolved.

--- 2018/day14/pt2.py
+++ 2018/day14/pt2.py
@@ -11,8 +11,9 @@ As it turns out, you got the Elves' plan backwards. They actually want to know h
 How many recipes appear on the scoreboard to the left of the score sequence in your puzzle input?
 """
 
-from collections import deque
 import timeit
+
+from blist import blist
 
 
 def run() -> None:
@@ -21,19 +22,29 @@ def run() -> None:
     """
     with open("./input.txt") as f:
         # Load data.
-        data = f.read().strip()
+        # data = "51589" #first appears after 9 recipes.
+        # data = "01245" #first appears after 5 recipes.
+        # data = "92510" #first appears after 18 recipes.
+        # data = "59414" #first appears after 2018 recipes.
+
         # data = "59414"
+        # data = "01245"
+
+        # data = "260321"  # 20M?
+
+        data = f.read().strip()
+        # data = "540391" # ???
+
+        data = [int(digit) for digit in data]
         sequence_length = len(data)
 
-        recipes = deque([3, 7])
+        recipes = blist([3, 7])
         offset1 = 0
         offset2 = 1
 
         recipes_count = 2
 
-        output_counter = 0
         while True:
-            output_counter += 1
             sum_ = recipes[offset1] + recipes[offset2]
             for digit in str(sum_):
                 recipes.append(int(digit))
@@ -42,24 +53,12 @@ def run() -> None:
             offset1 = (offset1 + recipes[offset1] + 1) % recipes_count
             offset2 = (offset2 + recipes[offset2] + 1) % recipes_count
 
-            sample = ""
-            for i in range(sequence_length + 1):
-                recipes.rotate(1)
-                sample += str(recipes[0])
-
-            sample = list(reversed(sample))
-            if sample[:-1] == data:
-                print(recipes_count - sequence_length + 1)
-                return
-            if sample[1:] == data:
+            if recipes[-sequence_length:] == data:
                 print(recipes_count - sequence_length)
                 return
-
-            recipes.rotate(-sequence_length - 1)
-
-            if output_counter == 100000:
-                print(f"{recipes_count}")
-                output_counter = 0
+            if recipes[-sequence_length - 1:-1] == data:
+                print(recipes_count - sequence_length - 1)
+                return
 
 
 print(f"{timeit.timeit(run, number=1)} sec")
